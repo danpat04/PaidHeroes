fcdec979fdaee6ecac317fadd7af411ab86fee45
danpat04 <plumpwesley@naver.com>
2019-03-02 13:37:45 +0900

    PaidHeroes Start

---
+++ PaidHeroes.Core/Hero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PaidHeroes.Core
+{
+    public class Hero
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; } = 1;
+
+        public Hero(string name, int level)
+        {
+            Name = name;
+            Level = level;
+        }
+    }
+}
